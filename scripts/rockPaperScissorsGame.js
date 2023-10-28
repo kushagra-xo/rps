523d019c9418f6616bd38d72f083b67b5615c14e
Kushagra J <>
2023-10-29 01:36:31 +0530

    redesigned: added css and refined more thins such as adding an information header instead of alerting

--- scripts/rockPaperScissorsGame.js
+++ scripts/rockPaperScissorsGame.js
@@ -7,16 +7,48 @@
 
 const selectionOptions = ["ROCK", "PAPER", "SCISSORS"]
 
+const rock = document.getElementById('rock')
+const paper = document.getElementById('paper')
+const scissors = document.getElementById('scissors')
+
+rock.addEventListener('click', () => handleButtonClick('ROCK'))
+paper.addEventListener('click', () => handleButtonClick('PAPER'))
+scissors.addEventListener('click', () => handleButtonClick('SCISSORS'))
+
+let score = 0;
+document.getElementById('score').textContent = "Score: " + score;
+
+let result = null;
+document.getElementById('result').textContent = "Result: " + result;
+
+let randomComputerSelection = null;
+document.getElementById('computerChoice').textContent = "Computer: " + selectionOptions[randomComputerSelection];
+
+function updateScore()
+{
+  document.getElementById('score').textContent = "Score: " + score;
+}
+function updateResult()
+{
+  document.getElementById('result').textContent = "Result: " + result;
+}
+
 function handleButtonClick(input) {
-  let randomComputerSelection = Math.floor(Math.random() * selectionOptions.length);
+  randomComputerSelection = Math.floor(Math.random() * selectionOptions.length);
   let playerSelectionLocal = selectionOptions.indexOf(input.toUpperCase());
   let computerLoseIndex = (playerSelectionLocal - 1 + selectionOptions.length) % selectionOptions.length;
 
   if (playerSelectionLocal == randomComputerSelection) {
-    alert(`RESULT: TIE\nCOMPUTER CHOSE: ${selectionOptions[randomComputerSelection]}`);
+    result = `TIE`
   } else if (randomComputerSelection == computerLoseIndex) {
-    alert(`RESULT: WIN\nCOMPUTER CHOSE: ${selectionOptions[randomComputerSelection]}`);
+    result = `WIN`
+    score++;
   } else {
-    alert(`RESULT: LOSE\nCOMPUTER CHOSE: ${selectionOptions[randomComputerSelection]}`);
+    result = `LOSE`
+    score--;
   }
+
+  updateScore();
+  updateResult();
+  document.getElementById('computerChoice').textContent = "Computer: " + selectionOptions[randomComputerSelection];
 }
