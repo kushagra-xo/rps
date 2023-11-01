5d19296c0872dcbfdff1377d03aaa3e6d4cf8f0e
Kushagra J <>
2023-11-01 16:55:59 +0530

    minor styling changes

--- scripts/rockPaperScissorsGame.js
+++ scripts/rockPaperScissorsGame.js
@@ -1,5 +1,4 @@
 const selectionOptions = ["ROCK", "PAPER", "SCISSORS"]
-
 const rock = document.getElementById('rock')
 const paper = document.getElementById('paper')
 const scissors = document.getElementById('scissors')
